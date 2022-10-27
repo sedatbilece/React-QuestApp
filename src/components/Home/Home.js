b5752a6835d532c105ee154251ee74b16a674b9a
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2022-10-27 11:39:39 +0300

    add materialUI , style home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,8 +3,9 @@ import Post from '../Post/Post';
 function Home() {
     return (
         <div className="container">
-        <h1>Home page</h1>  
+        
 
+        <Post />
         <Post />
         </div>
     );
--- src/components/Post/PostElement.js
+++ src/components/Post/PostElement.js
@@ -1,15 +1,63 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 
+
+    
 
 function PostElement({text, title}) {
 
-
+    const [expanded, setExpanded] = React.useState(false);
+  
+    const handleExpandClick = () => {
+      setExpanded(!expanded);
+    };
     return (
-        <div className="post-element">
-            <h3>{title}</h3>
-            <h5> {text}</h5>
+        
+            <Card sx={{ width: 800  }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            S
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={title}
+         
+      />
+      
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+         {text}
+        </Typography>
+      </CardContent>
+     
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          
+        </CardContent>
+      </Collapse>
+    </Card>
             
-        </div>
+       
     );
 }
 
